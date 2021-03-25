3120d8a1816426308fc3c0170b6f02c5390b23f7
nurahmetemin <nurahmedemin@gmai.com>
2021-03-25 07:24:25 -0700

    initial commit

---
+++ src/test/java/day1/Junit5_intro.java
@@ -0,0 +1,13 @@
+package day1;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit5_intro {
+
+    @Test
+    public void test(){
+        System.out.println("Learning Junit5");
+    }
+
+
+}
