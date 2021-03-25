e7f76e658dbffe85025df65d5af120e7bcb24f5d
nurahmetemin <nurahmedemin@gmai.com>
2021-03-25 07:40:14 -0700

    added second test for testing name starts with 'A'

--- src/test/java/day1/Junit5_intro.java
+++ src/test/java/day1/Junit5_intro.java
@@ -3,6 +3,7 @@ package day1;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Junit5_intro {
 
@@ -10,9 +11,16 @@ public class Junit5_intro {
     public void test(){
         System.out.println("Learning Junit5");
 
-
         assertEquals(1,2);
+        assertEquals(1,2, "Something is wrong");
 
+    }
+
+    @Test
+    public void test1(){
+        String name = "Nurahmet";
+        assertEquals('A',name.charAt(0));
+        //assertTrue(name.startsWith("A"));
 
     }
 
